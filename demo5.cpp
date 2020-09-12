5a52ae72c6ca218a3a007368ddbd4c1f561fd5eb
Nguyễn Hải Đăng <nguyenhaidangyq@gmail.com>
2020-09-12 08:39:23 +0700

    Nộp bài tập về nhà 1

---
+++ demo5.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+	
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+		int b;
+	printf("nhap b=");
+	scanf("%d",&b);
+	
+		int c;
+	printf("nhap c=");
+	scanf("%d",&c);
+	int M;
+	M=a;
+	if (M<b){
+		M=b;
+	}else{
+		
+	}
+	if (M<c){
+		M=c;
+	}else{
+		
+	}
+	printf("so lon nhat la: %d",M);
+}
